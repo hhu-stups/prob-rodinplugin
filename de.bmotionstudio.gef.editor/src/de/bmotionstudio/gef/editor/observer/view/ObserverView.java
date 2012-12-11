f015d66b9fcf7956f4a249f5c34fe0928961dece
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2012-12-11 12:02:24 +0100

    added some meta info

--- de.bmotionstudio.gef.editor/src/de/bmotionstudio/gef/editor/observer/view/ObserverView.java
+++ de.bmotionstudio.gef.editor/src/de/bmotionstudio/gef/editor/observer/view/ObserverView.java
@@ -1,3 +1,9 @@
+/** 
+ * (c) 2009 Lehrstuhl fuer Softwaretechnik und Programmiersprachen, 
+ * Heinrich Heine Universitaet Duesseldorf
+ * This software is licenced under EPL 1.0 (http://www.eclipse.org/org/documents/epl-v10.html) 
+ * */
+
 package de.bmotionstudio.gef.editor.observer.view;
 
 import org.eclipse.ui.IEditorPart;
