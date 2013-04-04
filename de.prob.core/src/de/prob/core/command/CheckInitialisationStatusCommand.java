9cfd3bf4aa2b40c2d91ae088060d6dd43b899293
Joy Clark <joyclark2004@msn.com>
2013-04-04 13:29:21 +0200

    PROBPLUGIN-67 changed the command so that it is compatible with the current probcli

--- de.prob.core/src/de/prob/core/command/CheckInitialisationStatusCommand.java
+++ de.prob.core/src/de/prob/core/command/CheckInitialisationStatusCommand.java
@@ -12,7 +12,7 @@ import de.prob.exceptions.ProBException;
 public final class CheckInitialisationStatusCommand extends
 		CheckBooleanPropertyCommand {
 
-	private static final String IS_INITIALISED_STATE = "isInitialisedState";
+	private static final String IS_INITIALISED_STATE = "initialised";
 
 	public static boolean isInitialized(final Animator a, final String stateId)
 			throws ProBException {
