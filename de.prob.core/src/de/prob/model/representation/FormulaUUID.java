7a61c13deff76696ef5f496bf949e629c0cb9f3c
Joy Clark <joyclark2004@msn.com>
2013-04-09 14:09:01 +0200

    PROBPLUGIN-69 Deleted old unnecessary model objects

--- de.prob.core/src/de/prob/model/representation/FormulaUUID.java
+++
@@ -1,6 +0,0 @@
-package de.prob.model.representation;
-
-public class FormulaUUID {
-	static int count = 0;
-	public final String uuid = "Formula_" + (++count);
-}
--- de.prob.core/src/de/prob/model/representation/IEntity.java
+++
@@ -1,9 +0,0 @@
-package de.prob.model.representation;
-
-import java.util.List;
-
-public interface IEntity {
-	public List<IEntity> getChildren();
-
-	public boolean hasChildren();
-}
