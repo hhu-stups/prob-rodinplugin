77273f335878dd0af268a2edca5d915d191c3326
Sebastian Krings <sebastian@krin.gs>
2013-06-25 13:40:43 +0200

    add "stop when all events are covered" option to model checking dialog

--- de.prob.core/src/de/prob/core/command/ConsistencyCheckingSearchOption.java
+++ de.prob.core/src/de/prob/core/command/ConsistencyCheckingSearchOption.java
@@ -14,11 +14,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ConsistencyCheckingSearchOption {
-			breadth_first_search(0, "Breadth First Search", false),
-			find_deadlocks(1,"Find Deadlocks", true),
-			find_invariant_violations(2,"Find Invariant Violations", true),
-			find_assertion_violations(3,"Find Theorem Violations", false),
-			inspect_existing_nodes(4,"Recheck existing states", false);
+	breadth_first_search(0, "Breadth First Search", false), find_deadlocks(1,
+			"Find Deadlocks", true), find_invariant_violations(2,
+			"Find Invariant Violations", true), find_assertion_violations(3,
+			"Find Theorem Violations", false), inspect_existing_nodes(4,
+			"Recheck Existing States", false), stop_at_full_coverage(5,
+			"Stop when all Events are Covered", false);
 
 	private final String text;
 	private final int pos;
