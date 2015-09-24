f5c9ccfb0faabb24ce1dafe21dbcb0417f289301
Sebastian Krings <sebastian@krin.gs>
2015-09-24 08:12:23 +0200

    fix extended timeout option of disprover

--- de.prob.eventb.disprover.ui/src/de/prob/eventb/disprover/ui/DisproverExtendedTimeoutTacticProvider.java
+++ de.prob.eventb.disprover.ui/src/de/prob/eventb/disprover/ui/DisproverExtendedTimeoutTacticProvider.java
@@ -1,45 +1,48 @@
 package de.prob.eventb.disprover.ui;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 
+import org.eclipse.swt.graphics.Image;
 import org.eventb.core.ast.Predicate;
-import org.eventb.core.seqprover.*;
+import org.eventb.core.seqprover.IProofTreeNode;
+import org.eventb.core.seqprover.IReasonerInput;
+import org.eventb.core.seqprover.ITactic;
 import org.eventb.core.seqprover.tactics.BasicTactics;
-import org.eventb.ui.prover.*;
+import org.eventb.ui.prover.DefaultTacticProvider;
+import org.eventb.ui.prover.IPredicateApplication;
+import org.eventb.ui.prover.ITacticApplication;
 
-import de.prob.eventb.disprover.core.*;
+import de.prob.eventb.disprover.core.Disprover;
+import de.prob.eventb.disprover.core.DisproverReasonerInput;
 
 public class DisproverExtendedTimeoutTacticProvider extends
 		DefaultTacticProvider {
-
-	protected boolean useContexts() {
-		return false;
-	}
-
-	protected static class MyPredicateApplication implements ITacticApplication {
-
-		private final IProofTreeNode node;
-
-		public MyPredicateApplication(IProofTreeNode node) {
-			this.node = node;
-		}
-
+	protected static class MyPredicateApplication implements
+			IPredicateApplication {
 		@Override
 		public ITactic getTactic(String[] inputs, String globalInput) {
-			return getTactic(node, globalInput, inputs);
-		}
-
-		public ITactic getTactic(IProofTreeNode node, String globalInput,
-				String[] inputs) {
-
 			IReasonerInput reasonerInput = new DisproverReasonerInput();
 			return BasicTactics.reasonerTac(
-					Disprover.createDisproverReasoner(), reasonerInput);
+					Disprover.createExtendedTimeoutDisproverReasoner(10),
+					reasonerInput);
 		}
 
 		@Override
 		public String getTacticID() {
 			return "de.prob.eventb.disprover.ui.disproverTactic";
+		}
+
+		@Override
+		public Image getIcon() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getTooltip() {
+			// TODO Auto-generated method stub
+			return null;
 		}
 
 	}
@@ -53,7 +56,7 @@ public class DisproverExtendedTimeoutTacticProvider extends
 			IProofTreeNode node, Predicate hyp, String globalInput) {
 
 		if (node != null && node.isOpen()) {
-			ITacticApplication application = new MyPredicateApplication(node);
+			ITacticApplication application = new MyPredicateApplication();
 			return Collections.singletonList(application);
 		}
 		return Collections.<ITacticApplication> emptyList();
