6fa399924d5af5713daa9e04421e373c49e926d6
Sebastian Krings <sebastian@krin.gs>
2016-02-03 14:30:12 +0100

    do not implement ITranslatableReasonerInput

--- de.prob.eventb.disprover.core/src/de/prob/eventb/disprover/core/DisproverReasonerInput.java
+++ de.prob.eventb.disprover.core/src/de/prob/eventb/disprover/core/DisproverReasonerInput.java
@@ -1,21 +1,17 @@
 package de.prob.eventb.disprover.core;
 
-import org.eventb.core.ast.FormulaFactory;
-import org.eventb.core.ast.ITypeEnvironment;
 import org.eventb.core.seqprover.IReasonerInput;
-import org.eventb.core.seqprover.ITranslatableReasonerInput;
 import org.eventb.core.seqprover.proofBuilder.ReplayHints;
 
 /**
  * Input for the Disprover. The Input keeps track of the {@link DisproverMode}
  * that the Disprover runs in. To prevent conditional statements in the
- * Disprover code, this method provides some helper mthod that return the
+ * Disprover code, this method provides some helper method that return the
  * appropriate values according to the mode.
  * 
  * @author jastram
  */
-public class DisproverReasonerInput implements IReasonerInput,
-		ITranslatableReasonerInput {
+public class DisproverReasonerInput implements IReasonerInput {
 	@Override
 	public void applyHints(ReplayHints renaming) {
 		// TODO Auto-generated method stub
@@ -31,18 +27,6 @@ public class DisproverReasonerInput implements IReasonerInput,
 	public boolean hasError() {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public ITypeEnvironment getTypeEnvironment(FormulaFactory arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IReasonerInput translate(FormulaFactory arg0) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
