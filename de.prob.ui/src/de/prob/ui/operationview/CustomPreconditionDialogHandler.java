ee6ab585413d9bcff114eeeb2a355b475b2c2f3b
Michael Leuschel <leuschel@cs.uni-duesseldorf.de>
2019-09-24 10:20:22 +0200

    update text referring to additional guard

--- de.prob.ui/src/de/prob/ui/operationview/CustomPreconditionDialogHandler.java
+++ de.prob.ui/src/de/prob/ui/operationview/CustomPreconditionDialogHandler.java
@@ -30,7 +30,7 @@ public class CustomPreconditionDialogHandler extends AbstractHandler implements
 
 		if (filtered.size() != 1) {
 			Logger.notifyUser(filtered.size()
-					+ " operations selected. An additional Guard can only be added to a single operation");
+					+ " operations selected. An additional Guard Constraint can only be added to a single operation");
 			return null;
 		}
 
