c244985440c0b3c93f48311a0e7dbd7194434ca6
dgelessus <dgelessus@users.noreply.github.com>
2022-12-05 14:23:17 +0100

    Remove no longer used units plugin code
    The units plugin has been disabled since 2019
    (47bc301e5842d69b74ca4c5824a85ae2575be185) and the Prolog side no longer
    supports it.

--- de.prob.units/src/de/prob/units/sc/UnitAttributeProcessorStatics.java
+++
@@ -1,9 +0,0 @@
-package de.prob.units.sc;
-
-public class UnitAttributeProcessorStatics {
-	static String translateEventBPragmaToBPragma(String eventBPragma) {
-		String bPragma = eventBPragma.replace("^", "**");
-		return bPragma;
-	}
-
-}
